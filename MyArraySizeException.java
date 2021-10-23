496f3f59b8b646db5a79140c6da681f9148bdbbd
elvi <elvi.garipova@gmail.com>
2021-10-23 16:53:41 +0300

    Initial commit.

---
+++ MyArraySizeException.java
@@ -0,0 +1,9 @@
+package lesson2;
+
+public class MyArraySizeException extends CustomException {
+
+    MyArraySizeException() {
+        super("Размерность массива должна быть [4 x 4]\n");
+    }
+}
+
---
+++ hw2.java
@@ -0,0 +1,47 @@
+package lesson2;
+
+public class hw2 {
+    public static void main(String[] args) {
+
+        String[][] correctMatrix = {
+                {"3", "7", "3", "1"},
+                {"1", "6", "8", "4"},
+                {"0", "5", "7", "2"},
+                {"0", "2", "7", "8"}
+        };
+        String[][] wrongSizeMatrix = {
+                {"1", "2", "0", "3"},
+                {"6", "4", "1", "5"},
+                {"9", "2", "7", "4"},
+                {"0", "2"}
+        };
+        String[][] wrongCharMatrix = {
+                {"1", "N", "2", "3"},
+                {"6", "1", "5", "7"},
+                {"2", "5", "9", "6"},
+                {"1", "9", "6", "5"}
+        };
+
+        try {
+            System.out.println("Задание 1:");
+            System.out.println("Сумма всех элементов массива равна " + Converter.strConverter(correctMatrix) + ".\n");
+        } catch (CustomException e) {
+            e.getMessage();
+        }
+
+        try {
+            System.out.println("Задание 2:");
+            System.out.println("Сумма всех элементов массива равна " + Converter.strConverter(wrongSizeMatrix) + ".\n");
+        } catch (CustomException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.println("Задание 3:");
+            System.out.println("Сумма всех элементов массива равна " + Converter.strConverter(wrongCharMatrix) + ".\n");
+        } catch (CustomException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
